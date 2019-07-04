6ea5b326784714621feefb1c6d674472321d254e
mstosio <michaustosio@gmail.com>
2019-07-04 21:17:15 +0200

    Updates

---
+++ precache-manifest.2602a8108ed3b0c1bf97e6694acdf2c0.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df33b5007c16f692047f03708f05c6fb",
+    "url": "/fitnessapp/index.html"
+  },
+  {
+    "revision": "fdad02e786220c641764",
+    "url": "/fitnessapp/static/css/2.b84cf735.chunk.css"
+  },
+  {
+    "revision": "47cc0bf73860681dc9d3",
+    "url": "/fitnessapp/static/css/main.dbd01882.chunk.css"
+  },
+  {
+    "revision": "fdad02e786220c641764",
+    "url": "/fitnessapp/static/js/2.26cdcf5d.chunk.js"
+  },
+  {
+    "revision": "47cc0bf73860681dc9d3",
+    "url": "/fitnessapp/static/js/main.0fc91876.chunk.js"
+  },
+  {
+    "revision": "78e69adb0d2e26275d33",
+    "url": "/fitnessapp/static/js/runtime~main.a5e5ec53.js"
+  },
+  {
+    "revision": "38ecc51a283f68c27d03493bf3d5beb2",
+    "url": "/fitnessapp/static/media/carbs.38ecc51a.svg"
+  },
+  {
+    "revision": "2e706318e95d7dce06dc9719b8a8d48c",
+    "url": "/fitnessapp/static/media/fats.2e706318.svg"
+  },
+  {
+    "revision": "4124805c0503dbfe42dd67d7f5715964",
+    "url": "/fitnessapp/static/media/montserrat-latin-100.4124805c.woff2"
+  },
+  {
+    "revision": "c8fb2f714bbc7bc3e8dfffa916b286dc",
+    "url": "/fitnessapp/static/media/montserrat-latin-100.c8fb2f71.woff"
+  },
+  {
+    "revision": "d1f3f2d02ee4d7d2d4b1ad865014f189",
+    "url": "/fitnessapp/static/media/montserrat-latin-100italic.d1f3f2d0.woff"
+  },
+  {
+    "revision": "e4bf47bd171a9b2a72dd84c58bf90edf",
+    "url": "/fitnessapp/static/media/montserrat-latin-100italic.e4bf47bd.woff2"
+  },
+  {
+    "revision": "444ae007121264bc1969d49b4031f9b2",
+    "url": "/fitnessapp/static/media/montserrat-latin-200.444ae007.woff2"
+  },
+  {
+    "revision": "edbce16a90aa22c297a0307b85789837",
+    "url": "/fitnessapp/static/media/montserrat-latin-200.edbce16a.woff"
+  },
+  {
+    "revision": "d7bbb730d9b5e11720b3eb32326dcca7",
+    "url": "/fitnessapp/static/media/montserrat-latin-200italic.d7bbb730.woff"
+  },
+  {
+    "revision": "f316c5d1ec40f3e68654c3f38b3999f3",
+    "url": "/fitnessapp/static/media/montserrat-latin-200italic.f316c5d1.woff2"
+  },
+  {
+    "revision": "0a7c6df06e85d978d096d4d18fd8d43d",
+    "url": "/fitnessapp/static/media/montserrat-latin-300.0a7c6df0.woff2"
+  },
+  {
+    "revision": "5e86df2cad22d2ef2b03516334afae5e",
+    "url": "/fitnessapp/static/media/montserrat-latin-300.5e86df2c.woff"
+  },
+  {
+    "revision": "37c74a8d2d0d36a0a2c6e9a37ee15b0c",
+    "url": "/fitnessapp/static/media/montserrat-latin-300italic.37c74a8d.woff"
+  },
+  {
+    "revision": "c076c4892bc7a4be7b9097e97a35012d",
+    "url": "/fitnessapp/static/media/montserrat-latin-300italic.c076c489.woff2"
+  },
+  {
+    "revision": "501ce09c42716a2f6e1503a25eb174c9",
+    "url": "/fitnessapp/static/media/montserrat-latin-400.501ce09c.woff2"
+  },
+  {
+    "revision": "f29d2b8559699b6beb5b29b25b8bc572",
+    "url": "/fitnessapp/static/media/montserrat-latin-400.f29d2b85.woff"
+  },
+  {
+    "revision": "22e7b04e5f2a901d49d4d342315a715a",
+    "url": "/fitnessapp/static/media/montserrat-latin-400italic.22e7b04e.woff"
+  },
+  {
+    "revision": "882908d9950d9c86ebd380877f293d95",
+    "url": "/fitnessapp/static/media/montserrat-latin-400italic.882908d9.woff2"
+  },
+  {
+    "revision": "991b453bf90a0980e78966d2af7e3d3a",
+    "url": "/fitnessapp/static/media/montserrat-latin-500.991b453b.woff"
+  },
+  {
+    "revision": "f0f2716c5fe401d175b88715e7d28685",
+    "url": "/fitnessapp/static/media/montserrat-latin-500.f0f2716c.woff2"
+  },
+  {
+    "revision": "4590ebba421b3288c305305d7fa7b504",
+    "url": "/fitnessapp/static/media/montserrat-latin-500italic.4590ebba.woff2"
+  },
+  {
+    "revision": "f3d41e4cdcc2314e49ddcea751d6f87f",
+    "url": "/fitnessapp/static/media/montserrat-latin-500italic.f3d41e4c.woff"
+  },
+  {
+    "revision": "15c24f7109941777774ddd2c636c6a50",
+    "url": "/fitnessapp/static/media/montserrat-latin-600.15c24f71.woff2"
+  },
+  {
+    "revision": "f6dc6096f48956908c1787d9a722570a",
+    "url": "/fitnessapp/static/media/montserrat-latin-600.f6dc6096.woff"
+  },
+  {
+    "revision": "02c4833312d94b1b0866f073023a250e",
+    "url": "/fitnessapp/static/media/montserrat-latin-600italic.02c48333.woff"
+  },
+  {
+    "revision": "6d10b80529d5c36c7c09fca7193af0fc",
+    "url": "/fitnessapp/static/media/montserrat-latin-600italic.6d10b805.woff2"
+  },
+  {
+    "revision": "79982cd1f74c6fa7451bf9b37ead09ff",
+    "url": "/fitnessapp/static/media/montserrat-latin-700.79982cd1.woff2"
+  },
+  {
+    "revision": "957e93fbbe131a59791cd820d98b7109",
+    "url": "/fitnessapp/static/media/montserrat-latin-700.957e93fb.woff"
+  },
+  {
+    "revision": "283438e9577fe6a684466bb100e105ec",
+    "url": "/fitnessapp/static/media/montserrat-latin-700italic.283438e9.woff2"
+  },
+  {
+    "revision": "ca627c5ccc65cf80c2ecaea44b997de9",
+    "url": "/fitnessapp/static/media/montserrat-latin-700italic.ca627c5c.woff"
+  },
+  {
+    "revision": "35386154b78d046218fc8f88a44ff515",
+    "url": "/fitnessapp/static/media/montserrat-latin-800.35386154.woff2"
+  },
+  {
+    "revision": "756655905d91b77960888262e7d58d35",
+    "url": "/fitnessapp/static/media/montserrat-latin-800.75665590.woff"
+  },
+  {
+    "revision": "a69f0add9d86c1a84311d7dd8693ba4a",
+    "url": "/fitnessapp/static/media/montserrat-latin-800italic.a69f0add.woff"
+  },
+  {
+    "revision": "e1b52a7bd83e2324db6d92bdc206844c",
+    "url": "/fitnessapp/static/media/montserrat-latin-800italic.e1b52a7b.woff2"
+  },
+  {
+    "revision": "186cae8091da578150d81958e217714a",
+    "url": "/fitnessapp/static/media/montserrat-latin-900.186cae80.woff"
+  },
+  {
+    "revision": "260c2ea3ef57feb82251952e605a36d5",
+    "url": "/fitnessapp/static/media/montserrat-latin-900.260c2ea3.woff2"
+  },
+  {
+    "revision": "43b527fe77254f97ea36e2b54e845ec4",
+    "url": "/fitnessapp/static/media/montserrat-latin-900italic.43b527fe.woff"
+  },
+  {
+    "revision": "d785fb9fc74588ffb7f306799709a97d",
+    "url": "/fitnessapp/static/media/montserrat-latin-900italic.d785fb9f.woff2"
+  },
+  {
+    "revision": "38e9447188fdbc27d542e5471f453c3c",
+    "url": "/fitnessapp/static/media/protein.38e94471.svg"
+  }
+]);
